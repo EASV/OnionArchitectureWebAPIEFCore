20ca3068f7d454b6f6e0bbc5d124ab07f0411e62
Lars Juul <ljb@easv.dk>
2021-08-24 09:40:23 +0200

    Adding second Infrastructure Repostiory

--- InnoTech.VideoApplication2021.UI/Program.cs
+++ InnoTech.VideoApplication2021.UI/Program.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.Design;
 using InnoTech.VideoApplication2021.Domain.IRepositories;
 using InnoTech.VideoApplication2021.Domain.Services;
 using InnoTech.VideoApplication2021.Infrastructure.DataAccess.Repositories;
+using InnoTech.VideoApplication2021.SQL.Repositories;
 using InnotTech.VideoApplication2021.Core.IServices;
 
 namespace InnoTech.VideoApplication2021.UI
@@ -12,7 +13,8 @@ namespace InnoTech.VideoApplication2021.UI
         static void Main(string[] args)
         {
             //Cheapish DI (Dependency Injection)
-            IVideoRepository repo = new VideoRepositoryInMemory();
+            //IVideoRepository repo = new VideoRepositoryInMemory();
+            IVideoRepository repo = new VideoRepository();
             IVideoService service = new VideoService(repo);
             
             var menu = new Menu(service);
